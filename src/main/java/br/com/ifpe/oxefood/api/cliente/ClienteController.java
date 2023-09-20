49d7ee7f59db8a8d5c6bcdbf2e6cb8b17eed0859
Eduarda Santana <edu.ardasantana@outlook.com>
2023-09-19 21:02:10 -0300

    Implementando a exclusão

---
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -0,0 +1,66 @@
+package br.com.ifpe.oxefood.api.cliente;
+
+import javax.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+
+@RestController
+@RequestMapping("/api/cliente")
+@CrossOrigin
+public class ClienteController {
+
+   @Autowired
+   private ClienteService clienteService;
+
+   @PostMapping
+   public ResponseEntity<Cliente> save(@RequestBody @Valid ClienteRequest request) {
+
+       Cliente cliente = clienteService.save(request.build());
+       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+   }
+
+   @GetMapping
+    public List<Cliente> findAll() {
+  
+        return clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable Long id) {
+
+        return clienteService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+   public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
+
+       clienteService.update(id, request.build());
+       return ResponseEntity.ok().build();
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+       clienteService.delete(id);
+       return ResponseEntity.ok().build();
+   }
+
+
+
+}
